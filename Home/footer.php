5b85528eba9262057b445daced778ce41de94679
Anbcdeptraivkl <hothaian911@gmail.com>
2020-06-25 20:39:38 +0700

    Post and Footer from Anbcdeptraivkl

---
+++ Home/footer.php
@@ -0,0 +1,16 @@
+<footer class="footer">
+    <div class="slogan">The World most Interesting Blog!</div>
+    <div class="logo">
+        <img src="images/logo-placeholder.png">
+    </div>
+    <hr />
+    <div class="bottom">
+        <div class="social-group">
+            <div class="social-icon facebook"></div>
+            <div class="social-icon twitter"></div>
+            <div class="social-icon linked"></div>
+            <div class="social-icon youtube"></div>
+        </div>
+        <div class="copyrights">Made by Anbcdeptraivkl and Cong Dat © Copyrights 2020</div>
+    </div>
+</footer>
---
+++ Home/post.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Post</title>
+    <link href="css/reset.css" type="text/css" rel="stylesheet">
+    <link href="css/shared.css" type="text/css" rel="stylesheet">
+    <link href="css/post.css" type="text/css" rel="stylesheet">
+    <link href="css/footer.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+    <div class="hero top-hero">
+        <div class="text">
+            <h2 class="title">Title</h2>
+            <div class="description">
+                <div class="circle-img author-avatar">
+                    <img src="images/avatar.png">
+                </div>
+                <div class="details">
+                    <p class="author">Author</p>
+                    <p class="date">Sun, 01/01/2000</p>
+                </div>
+            </div>
+        </div>
+        <div class="circle-img post-thumb">
+            <img src="images/post.png">
+        </div>
+    </div>
+    <section class="post-content">
+        <p>As businesses adjust to the new normal of working remotely, there is more to consider than the technology. We must also figure out how to ensure all of our team members feel included.</>
+        <p>People who are quiet in an in-person meeting will be almost non-existent in an online meeting, which means that being an ally is even more important in a remote work environment. We must do all we can to draw the introverted, voiceless, and unsung team members out so we don’t lose their great ideas and — more importantly — so we don’t lose their trust and respect.</p>
+        <h3>What does it take to be an ally?</h3>
+        <p>An ally <a href="https://www.merriam-webster.com/dictionary/ally">is defined as</a> a person who provides assistance and support in an ongoing effort. </p>
+        <p>So what does it take to be an ally? I believe there are phases to becoming an ally. From advocacy to support to coexistence, each phase requires you to actively engage and display a level of understanding you may not have displayed in the past.</p>
+        <p>Advocacy is the process of supporting a cause, or in this case, a person. It is not an overnight process; it is ongoing, especially in a remote work environment. </p>
+        <p>Let me be clear: Advocacy is not speaking for someone. No one is asking for that.  What unsung team members need is access to the room to speak for themselves. It is all about using your influence to help others. Initially, you may need to speak up more often for others to get people with influence to listen.</p>
+        <p>Here are some tips to help you advocate for colleagues while working remotely.</p>
+        <h3>1. Check in often</h3>
+        <p>In a remote work environment, it’s hard to see if someone is uncomfortable in a meeting — or if they are trying to add to the conversation but cannot get a word in on a teleconference. Reach out to your team members individually to understand their comforts and discomforts with the current work situation.  </p>
+        <p>It does not take a manager or supervisor to care about the needs of a team. Encourage team members to check in with one another. Even though companies are beginning to reopen, there are some that will remain remote indefinitely, so we must be prepared. </p>
+        <p>As time passes and we remain quarantined, I try to reach out to my friends. Interaction is good for us all. The same principle applies for your team. </p>
+        <p>Speaking with other team members helps to relieve the stress of being inside most of the day and will also remove their anxiety about speaking up on teleconferences or video calls. Just knowing that someone else is feeling the same goes a long way.</p>
+        <h3>2. Echo input from others in meetings</h3>
+        <p>In 2016, <a href="https://www.washingtonpost.com/news/powerpost/wp/2016/09/13/white-house-women-are-now-in-the-room-where-it-happens/">The Washington Post reported a story</a> of women in the Obama Administration using a strategy called amplification to echo the good ideas that the women at the table voiced but that seemed to get overlooked. This happens all the time. </p>
+        <p>Meetings online can get chaotic when everyone is trying to speak at the same time. If you hear your usually unheard colleague make a good point, echo them and do not forget to give them credit for the great idea. </p>
+        <p>During these difficult times of unrest, your black team members may not only feel overlooked but also shut out of conversations with their team. This can be due to their white counterparts not knowing what to say, so they do not interact at all. Or it could be because they just do not feel like being overly accommodating due to current circumstances. Amplification can still work.</p>
+        <p>You could even consider amplifying an idea that did not get traction previously but that still applies now. Bring up the idea to the team and be sure to cite the original author by name. She may not feel 100% due to the current climate, but she will appreciate your support.</p>
+        <h3>3. Notice and correct instances of unconscious bias</h3>
+        <p>Despite our best intentions, we are all subject to biases. When you see instances of unconscious (or conscious) bias in your organization, act. Here are three suggestions for how you can correct instances of unconscious bias:</p>
+        <ul>
+            <li>Restructure communication channels to include video so everyone can be seen in the meetings and you can pay closer attention to who speaks — as well as to who is often interrupted in meetings. If any biases are noticed, immediately discuss the incident and take corrective action. </li>
+            <li>Increase your network. Take this opportunity to interact with those who you have not gotten to know on your team. It’s easy to give your attention exclusively to those you already know. Challenge yourself to get to know others. Pick someone on your team or in your organization who you don’t normally talk to and check in with them.</li>
+            <li>Since we are working in remote environments for extended periods of time (and some indefinitely), now is the best time to practice unbiased interviewing techniques. Work with your People Ops team to remove the names from resumes when they are reviewed to ensure each candidate has an equal opportunity to gain employment without bias.</li>`
+        </ul>
+        <p>There is no excuse for allowing bias to live freely in your organization. If you feel that biases are causing strife on your team or in your company, allocate funds for culture training and team building immediately. Most training can be done in remote settings, so you don’t have to wait.</p>
+        <h3>Leading with empathy</h3>
+        <p>Teams operate at their best when all members are fully engaged and contributing to solutions. This can happen in a remote working environment just as it has in person in the past. It simply takes empathy, intention, and a little extra time.  </p>
+        <p>We must remember that there are different circumstances affecting all of us right now. We are in the midst of a pandemic, and team members may be affected in different ways. Some may be struggling with family members and the illness, and others may have friends who have lost their lives. You don’t know what you don’t know. </p>
+        <p>Emotions are high from racial unrest as well. The way you work with your team will take some thought and effort, and a one-size-fits-all approach won’t work. Take the time to understand what each individual needs and why your advocacy may be needed in the first place.</p>
+        <p>Advocacy is not about forcing the issue; both parties have to be willing participants. It is not an easy task and should not be entered into lightly, but careers and, in most cases, emotional health are at stake.</p>
+    </section>
+    <section class="rec-posts">
+        <div class="separator rec-sep">
+            <hr />
+            <div class="text">RECOMMENDED</div>
+        </div>
+        <div class="grid-list articles">
+            <div class="card article">
+                <div class="thumb"></div>
+                <h3 class="title">Another Post that you may Care about</h3>
+            </div>
+            <div class="card article">
+                <div class="thumb"></div>
+                <h3 class="title">Another Post that you may Care about</h3>
+            </div>
+            <div class="card article">
+                <div class="thumb"></div>
+                <h3 class="title">Another Post that you may Care about</h3>
+            </div>
+            <div class="card article">
+                <div class="thumb"></div>
+                <h3 class="title">Another Post that you may Care about</h3>
+            </div>
+        </div>
+    </section>
+
+    <!--Footer-->
+    <?php include('footer.php');?>
+</body>
+</html>
