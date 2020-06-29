146f70d86c300577579dc87df90e3c5b533101af
DatHC <huynhcongdat1999@gmail.com>
2020-06-29 12:52:55 +0700

    index styling and effects

--- Home/index.php
+++ Home/index.php
@@ -49,8 +49,22 @@
 		</ul>
 	</header>
 	<!-- Introduce -->
-	<div class="introduce">
-		<h3><span class="M">M</span>emory</h3>
+	<div class="introduce" data-aos="flip-left"
+     data-aos-easing="ease-out-cubic"
+     data-aos-duration="2000">
+        <div class="sign">
+        	<h3 data-aos="zoom-in" data-aos-delay="200">
+				<span class="M fast-flicker">M</span>
+				<span class="E">e</span>
+				<span class="m">m</span>
+				<span class="O">o</span>
+				<span class="R flicker">r</span>
+				<span class="Y">y</span>
+		    </h3>
+        </div>
+		<div class="shape rellax">
+			<img src="../Home/images/flight.png" alt="">
+		</div>
 	</div>
 	<!-- End Introduce -->
 
@@ -261,5 +275,9 @@
 </script>
 
 <script src="js/script.js"></script>
+<script src="js/parallax.min.js"></script>
+<script>
+  var rellax = new Rellax('.rellax');
+</script>
 </body>
 </html>
